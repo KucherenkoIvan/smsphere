2f0f5bd52c6bb8a32adf9905d0a7dceb520f60a4
Ivan Kucherenko <i.kucherenko@1er.app>
2024-11-18 12:39:33 +0500

    feat: smarter routing & logs on start

--- internal/http/http.go
+++ internal/http/http.go
@@ -8,18 +8,44 @@ import (
 )
 
 const (
-	_ATTEMPTS = 5
+	_ATTEMPTS = 25
 	PORT      = 8080
 )
 
+var handlers map[string]http.HandlerFunc
+
+func getRouteKey(method string, prefix string) string {
+	return fmt.Sprintf("%s %s", method, prefix)
+}
+
+func route(method string, prefix string, handler http.HandlerFunc) {
+	handlers[getRouteKey(method, prefix)] = handler
+
+	http.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+		key := getRouteKey(r.Method, prefix)
+		handler := handlers[key]
+		if handler == nil {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		} else {
+			handler(w, r)
+		}
+	})
+}
+
 func Start() {
-	http.HandleFunc("/app/info", controllers.AppInfo)
-	http.HandleFunc("/api/app/counter", controllers.Counter)
+	handlers = make(map[string]http.HandlerFunc)
+
+	route("GET", "/app/info", controllers.AppInfo)
+	route("GET", "/api/app/counter", controllers.Counter)
+	route("POST", "/api/app/auth/login", controllers.Login)
+	route("POST", "/api/app/auth/register", controllers.Register)
 
 	var error error
 	for i := 0; i < _ATTEMPTS; i++ {
-		address := fmt.Sprintf("%s%d", ":", PORT+i)
+		address := fmt.Sprintf("%s%d", "localhost:", PORT+i)
+		log.Printf("Starting http server on %s...", address)
 		error = http.ListenAndServe(address, nil)
+		log.Printf("Error %s", error)
 	}
 
 	log.Fatal(error)
